44afe463dc648c9c100527bff899a2a272b78247
Mihai Voina <mihaitza1058@yahoo.com>
2022-07-05 11:05:10 +0300

    Hi

---
+++ Patterns/Memento/Caretaker.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+namespace MementoDesignPattern
+{
+    public class Caretaker
+    {
+        private List<Memento> ledTvList = new List<Memento>();
+
+        public void AddMemento(Memento m)
+        {
+            ledTvList.Add(m);
+            Console.WriteLine("LED TV's snapshots Maintained by CareTaker :" + m.GetDetails());
+        }
+
+        public Memento GetMemento(int index)
+        {
+            return ledTvList[index];
+        }
+    }
+}
+
+
+
---
+++ Patterns/Memento/Originator.cs
@@ -0,0 +1,26 @@
+namespace MementoDesignPattern
+{
+    public class Originator
+    {
+        public LEDTV ledTV;
+
+        public Memento CreateMemento()
+        {
+            return new Memento(ledTV);
+        }
+
+        public void SetMemento(Memento memento)
+        {
+            ledTV = memento.ledTV;
+        }
+
+        public string GetDetails()
+        {
+            return "Originator [ledTV=" + ledTV.GetDetails() + "]";
+        }
+    }
+}
+
+
+
+
